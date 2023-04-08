602b15098f55f09216e16543e83384f8411700ff
jay75chauhan <chauhan75jayk@gmail.com>
2023-04-08 12:06:36 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className=" flex flex-col text-white items-center justify-center h-screen px-2 ">
+      <h1 className="text-5xl font-bold mb-20">HomePage</h1>{" "}
+    </div>
+  );
+}
+
+export default HomePage;
